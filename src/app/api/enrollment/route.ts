43f13537c068123078c775872fcb81c2c6cb4333
HwiGyoung Lee <ter0000@naver.com>
2025-04-18 16:44:07 +0900

    [edit] : img 의 src가 사라지는 문제 해결

--- src/app/api/enrollment/route.ts
+++ src/app/api/enrollment/route.ts
@@ -1,9 +1,9 @@
-import { sanitizeHTML } from "./../../../utils/sanitizer";
 import { NextResponse, NextRequest } from "next/server";
 import { adminDb, adminStorage } from "../firebaseAdmin";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { v4 as uuidv4 } from "uuid";
+import { sanitizeHTML } from "@/utils/sanitizer";
 
 interface uploadedImages {
   id: string;
