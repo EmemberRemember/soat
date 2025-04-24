b1481016e0f93f13354e28d53ec44caf14e32bc9
HwiGyoung Lee <ter0000@naver.com>
2025-04-24 17:24:34 +0900

    [add] : 서버에서 performanceData 객체를 받았을 때 사용하는 필드만 enrollEditSlice에 dispatch 하는 hook

---
+++ src/hooks/useSetEditEnrollData.ts
@@ -0,0 +1,18 @@
+//hooks/useSetEditEnrollData.ts
+import { PerformanceDataWithStatus } from '@/components/manager/Performance';
+import { setEnrollEditData } from '@/redux/slices/enrollEditSlice';
+import { EnrollStep } from '@/types/enrollment';
+import { useDispatch } from "react-redux";
+
+export default function useSetEditEnrollData(performanceData: PerformanceDataWithStatus) {
+    
+    const dispatch = useDispatch();
+    
+    const setEditEnrollData = () => {
+        const { status, ...enrolldata } =  {
+            isDirty : false, step:EnrollStep.EnrollPerformance, invalidField : "" , ...performanceData } 
+         dispatch(setEnrollEditData(enrolldata))
+    }
+
+    return {setEditEnrollData}
+}
