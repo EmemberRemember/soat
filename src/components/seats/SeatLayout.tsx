acd3f2a5c21f84beb3c89bdebf7e993b0cdf2cc0
HwiGyoung Lee <ter0000@naver.com>
2025-04-29 14:07:07 +0900

    [edit] : 에러 메세지 폰트트 색상 변경

--- src/components/seats/SeatLayout.tsx
+++ src/components/seats/SeatLayout.tsx
@@ -111,7 +111,7 @@ export default function SeatLayout({
 
   const errorInfo = () => {
     return (
-      <section className="border-t-2 py-2">
+      <section className="border-t-2 py-2 text-flesh-500">
         <h3 className="font-bold">Error</h3>
         <p>{bookingError}</p>
       </section>
