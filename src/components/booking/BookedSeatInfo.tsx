d143db619dc263df3a0194d41077ce45bc3e55b4
HwiGyoung Lee <ter0000@naver.com>
2025-04-29 18:40:08 +0900

    [add] : 입금 상태를 변경하는 버튼 추가

--- src/components/booking/BookedSeatInfo.tsx
+++ src/components/booking/BookedSeatInfo.tsx
@@ -1,9 +1,12 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { bookResultType } from "@/types/reservation";
 import { PerformanceData } from "@/app/api/performance/route";
 import BookingErrorInfo from "./BookingErrorInfo";
 import { OccupiedSeat } from "@/types/performance";
+import { Button } from "../controls/Button";
+import axios from "axios";
+import Loading from "../Loading";
 
 interface Props {
   selectedSeatLabel: string;
@@ -13,6 +16,7 @@ interface Props {
   performanceTime: string;
   bookingData: bookResultType | null;
   bookingError: string | null;
+  performanceId: string;
 }
 
 const BookingStatus = {
@@ -29,9 +33,60 @@ export default function BookedSeatInfo({
   performanceTime,
   bookingData,
   bookingError,
+  performanceId,
 }: Props) {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const putReservationStatus = async (
+    reservationId: string,
+    performanceId: string,
+    status: "booked" | "pending",
+    performanceDate: string,
+    performanceTime: string
+  ) => {
+    try {
+      setLoading(true);
+      const response = await axios.put(
+        `/api/reservation/book/${reservationId}`, // API 엔드포인트 URL
+        {
+          performanceId: performanceId, // 요청 본문에 데이터 포함
+          status: status,
+          performanceDate: performanceDate,
+          performanceTime: performanceTime,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json", // 요청 헤더 설정 (JSON 형식으로 데이터 전송)
+          },
+        }
+      );
+
+      console.log("입금 상태 변경 성공:", response.data);
+      return response.data; // 성공 시 응답 데이터 반환 (선택 사항)
+    } catch (error) {
+      console.error("입금 상태 변경 실패:", error); // 에러를 다시 던져서 호출하는 쪽에서 처리하도록 함
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleOnClickUpdateReservationStatus = async () => {
+    const reservationId = selectedSeatItem.reservationId;
+    const status = "booked";
+    const data = await putReservationStatus(
+      reservationId,
+      performanceId,
+      status,
+      performanceDate,
+      performanceTime
+    );
+  };
+
   return (
-    <article>
+    <article className="relative">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        {loading && <Loading />}
+      </div>
       <h2 className="text-2xl font-bold text-center">
         좌석 {selectedSeatLabel} 정보
       </h2>
@@ -64,7 +119,7 @@ export default function BookedSeatInfo({
         <BookingErrorInfo bookingError={bookingError} />
       ) : (
         <>
-          <section className="border-t-2 py-2">
+          <section className="border-t-2 py-2 relative">
             <h3 className="font-bold">예약 정보</h3>
             <dl className="flex flex-wrap items-center justify-center">
               <dt className="basis-[30%] text-gray-500 my-0.5">예약자명</dt>
@@ -95,6 +150,13 @@ export default function BookedSeatInfo({
                   .join(", ")}
               </dd>
             </dl>
+            <Button
+              className="absolute bottom-2 right-0"
+              type="button"
+              onClick={handleOnClickUpdateReservationStatus}
+            >
+              예약 상태 변경
+            </Button>
           </section>
 
           <section className="border-t-2 py-2">
