66cec8ab97fb8e0b6e2747c0aaa65ebcf70a1081
jini0012 <jinhui.ha1208@gmail.com>
2025-04-29 14:58:25 +0900

    [add] ToastContainer에 Zoom 전환 효과 추가 및 showToast 함수에 goto 타입 추가

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -3,15 +3,26 @@ import { toast, ToastPosition } from "react-toastify";
 
 export function showToast(
   message: string,
-  type: "success" | "error" | "info" = "info"
+  type: "success" | "error" | "goto" | "info" = "info",
+  onClose?: () => void
 ) {
   const toastCustomStyle = {
-    position: "top-right" as ToastPosition,
-    autoClose: 2000,
+    role: "alert",
+    position: "bottom-right" as ToastPosition,
+    autoClose: 1000,
     hideProgressBar: true,
+    pauseOnFocusLoss: false,
+    closeButton: false,
   };
 
   if (type === "info") toast(message, toastCustomStyle);
   else if (type === "success") toast.success(message, toastCustomStyle);
   else if (type === "error") toast.error(message, toastCustomStyle);
+  else if (type === "goto")
+    toast(message, {
+      ...toastCustomStyle,
+      position: "top-center" as ToastPosition,
+      hideProgressBar: false,
+      onClose: onClose,
+    });
 }
